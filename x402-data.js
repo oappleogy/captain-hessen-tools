0e51fb35e172c27289bb5e82657fc05787e137d2
captainhessen <captain@192.168.0.102>
2026-08-12 02:13:11 +0800

    add x402 agent payments investment tracker

---
+++ x402-data.js
@@ -0,0 +1,31 @@
+window.X402_DATA = {
+  "generated_utc": "2026-08-11T18:07:09.819229Z",
+  "source": "x402scan / x402endpoint / BaseAlpha Research",
+  "market_30d": {
+    "transactions": 9071339,
+    "volume_usd": 896356.32,
+    "buyers": 20794,
+    "sellers": 68834,
+    "avg_payment_usd": 0.0988
+  },
+  "market_24h": {
+    "transactions": 211906,
+    "volume_usd": 46025.86
+  },
+  "leader": {
+    "name": "BlockRun",
+    "transactions": 6332276,
+    "volume_usd": 165388.08,
+    "buyers": 887
+  },
+  "endpoint_catalog": 19337,
+  "authenticity": {
+    "window_days": 280,
+    "nominal_transactions": 136708672,
+    "nominal_volume_usd": 44121383.81,
+    "fictitious_share": 21.2,
+    "linked_cluster_share": 63.78,
+    "named_service_lower_bound_usd": 187861.35,
+    "unproven_manufactured_upper_bound_usd": 20258746.09
+  }
+};
